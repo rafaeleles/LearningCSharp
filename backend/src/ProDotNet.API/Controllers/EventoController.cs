35466e880abe66ab063d267376f3c1638d9ed9bf
Rafael de Castro Leles <rafael.leles@mq.local>
2023-08-17 15:54:28 -0300

    C#

---
+++ backend/src/ProDotNet.API/Controllers/EventoController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProDotNet.API.Models;
+using ProDotNet.API.Data;
+
+namespace ProDotNet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[]{
+              new Evento() {
+                  EventoId = 1,
+                  Tema = "oi",
+                  Local = "Belo Horizonte",
+                  Lote = "1° Lote",
+                  QtdPessoas = 250,
+                  DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                  ImagemURL = "foto.png"
+              },
+              new Evento() {
+                  EventoId = 2,
+                  Tema = "ola",
+                  Local = "São Paulo",
+                  Lote = "2° Lote",
+                  QtdPessoas = 251,
+                  DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                  ImagemURL = "foto1.png"
+
+              }
+            };
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de put com id = {id}";
+        }
+    }
+}
---
+++ backend/src/ProDotNet.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProDotNet.API.Models;
+
+namespace ProDotNet.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Evento> Evento { get; set; }
+    }
+}
